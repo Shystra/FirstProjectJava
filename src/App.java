4bcf02fc98119f182958118b23c442328cd7191c
Shystra <jlsp.2019@hotmail.com>
2024-01-14 17:47:57 -0300

    test

--- src/App.java
+++ src/App.java
@@ -1,15 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {   
+        String completeName;
+        completeName = "name";
+        System.out.println(completeName);
         
-        // Apenas tamanho de algumas variaveis
-        byte smallNumber = 127;
-        short shortNumber = 32767;
-        int intNumber = 2147483647;
-        long longNumber = 92233720368547700l;
-
-        System.out.println(smallNumber);
-        System.out.println(shortNumber);
-        System.out.println(intNumber);
-        System.out.println(longNumber);
     }
 }
