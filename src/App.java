0682c99d1e98296d0395d356a2fa0eae5c64c544
Shystra <jlsp.2019@hotmail.com>
2024-01-14 17:39:00 -0300

    first commit.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {   
+        
+        // Apenas tamanho de algumas variaveis
+        byte smallNumber = 127;
+        short shortNumber = 32767;
+        int intNumber = 2147483647;
+        long longNumber = 92233720368547700l;
+
+        System.out.println(smallNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
     }
 }
